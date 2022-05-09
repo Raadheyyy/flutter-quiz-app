85ef288590e080ba957795f6b39719316df7a203
Raadheyyy <radheykhandelwal2000@gmail.com>
2022-05-09 18:58:38 +0530

    flutter quiz app

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import './quiz.dart';
+import './result.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 7},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 8},
+        {'text': 'Elephant', 'score': 4},
+        {'text': 'Lion', 'score': 10},
+      ],
+    },
+    {
+      'questionText': 'Who is your favourite?',
+      'answers': [
+        {'text': 'Person 1', 'score': 3},
+        {'text': 'Person 2', 'score': 8},
+        {'text': 'Person 3', 'score': 4},
+        {'text': 'Person 4', 'score': 2},
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex += 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Title Text'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions)
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
---
+++ lib/result.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetQuizFunction;
+  Result(this.resultScore, this.resetQuizFunction);
+
+  String get resultPhrase {
+    String resultText = 'You did it!';
+    if (resultScore <= 8) {
+      resultText = 'Light hearted';
+    } else if (resultScore <= 12) {
+      resultText = 'A little light hearted';
+    } else if (resultScore <= 16) {
+      resultText = "damn!";
+    } else {
+      resultText = "Heavy heavy hearted";
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(children: <Widget>[
+      Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+      FlatButton(
+          onPressed: resetQuizFunction,
+          textColor: Colors.green,
+          child: Text('Restart Quiz'))
+    ]));
+  }
+}
